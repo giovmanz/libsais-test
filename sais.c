50290a225fa07d5d5c9769ec28de1275a08e7b66
giovanni <giovanni.manzini@gmail.com>
2026-07-08 12:17:40 +0200

    added 16bit input for sais

--- sais.c
+++ sais.c
@@ -1,20 +1,15 @@
-/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
-   Suftest3.c
-   Written by G. Manzini using the suftest.c code 
-   by N. Jesper Larsson (Copyright N. Jesper Larsson 1999) 
-  
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   
    Program to test suffix sorting and LCP building  
-   Reads a sequence of bytes from
-   a file and calls the qsufsort algorithm described in the paper
-   "Faster Suffix Sorting" by N. Jesper Larsson (jesper@cs.lth.se) and Kunihiko
-   Sadakane (sada@is.s.u-tokyo.ac.jp). The source code of the qsufsort 
-   algorithm is in the file qsufsort.c.
+   Reads a sequence of unsigned bytes/short/int from
+   a file and compute SA and LCP array
+   here using the sais+plcp (from libsais) algorithm  
 
-   This software may be used freely for any purpose. However, when distributed,
-   the original source must be clearly stated, and, when the source code is
-   distributed, the copyright notice must be retained and any alterations in
-   the code must be clearly marked. No warranty is given regarding the quality
-   of this software.
+   Note This implementationcan work with 32 bits for inputs of size ar most 2**31-1, 
+   for larger files we need to switch to 64 bits (for both SA and LCP)
+   Space usage 
+     8bit input:  T + SA + LCP: 9n bytes 32bit, 17n 64bit
+    16bit input: T + SA + LCP: 10n bytes 32bit, 18n 64bit (to be ckecked)
+   Runinng time: truly linear 
    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
 #define _GNU_SOURCE
 #include <stdlib.h>
@@ -25,6 +20,8 @@
 #include <sys/times.h>
 #include "include/libsais.h"
 #include "include/libsais64.h"
+#include "include/libsais16.h"
+#include "include/libsais16x64.h"
 
 
 typedef uint8_t  uchar_t;
@@ -39,10 +36,16 @@ typedef int32_t  idx_t;
 int64_t libsais64(const uint8_t * T, int64_t * SA, int64_t n, int64_t fs, int64_t * freq);
 int64_t libsais64_plcp(const uint8_t * T, const int64_t * SA, int64_t * PLCP, int64_t n);
 int64_t libsais64_lcp(const int64_t * PLCP, const int64_t * SA, int64_t * LCP, int64_t n);
+int64_t libsais16x64(const uint16_t * T, int64_t * SA, int64_t n, int64_t fs, int64_t * freq);
+int64_t libsais16x64_plcp(const uint16_t * T, const int64_t * SA, int64_t * PLCP, int64_t n);
+int64_t libsais16x64_lcp(const int64_t * PLCP, const int64_t * SA, int64_t * LCP, int64_t n);
 #else
 int32_t libsais(const uint8_t * T, int32_t * SA, int32_t n, int32_t fs, int32_t * freq);
 int32_t libsais_plcp(const uint8_t * T, const int32_t * SA, int32_t * PLCP, int32_t n);
 int32_t libsais_lcp(const int32_t * PLCP, const int32_t * SA, int32_t * LCP, int32_t n);
+int32_t libsais16(const uint16_t * T, int32_t * SA, int32_t n, int32_t fs, int32_t * freq);
+int32_t libsais16_plcp(const uint16_t * T, const int32_t * SA, int32_t * PLCP, int32_t n);
+int32_t libsais16_lcp(const int32_t * PLCP, const int32_t * SA, int32_t * LCP, int32_t n);
 #endif
 
 #define DBL_CLK_TCK ((double) sysconf(_SC_CLK_TCK)) // clocks x secs 
@@ -85,7 +88,7 @@ void write_lcp(char *filename, idx_t *p, idx_t n)
 
 int main(int argc, char *argv[])
 {
-   uchar_t *x;
+   void *x;
    idx_t  *p, n;
    int c;
    clock_t end_time,start_time;
@@ -95,6 +98,7 @@ int main(int argc, char *argv[])
    extern int optind, optopt;
    char *fnam;
    FILE *f;
+   int input_is_16bit = 0;
 
   /* ------------ set default values ------------- */
   char *sa_filename = NULL;
@@ -102,13 +106,15 @@ int main(int argc, char *argv[])
   int compute_avg_lcp = 0;
 
   /* ------------- read options from command line ----------- */
-  while ((c=getopt(argc, argv, "vw:W:a")) != -1) {
+  while ((c=getopt(argc, argv, "vw:W:ax")) != -1) {
     switch (c) 
       {
       case 'w':
         sa_filename = optarg; break;
       case 'W':
         lcp_filename = optarg; break;
+      case 'x':
+        input_is_16bit = 1; break;
       // case 't':
       //   NumTreads = atoi(optarg); break;
       case 'v':
@@ -124,10 +130,10 @@ int main(int argc, char *argv[])
   if(optind<argc)
     fnam=argv[optind];
   else {
-    fprintf(stderr, "Usage:\n\t%s [-w safile][-v][-a] ",argv[0]);
-    fprintf(stderr, " file\n\n");
+    fprintf(stderr, "Usage:\n\t%s [-w safile][-W lcpfile][-x][-v][-a] file\n\n",argv[0]);
     fprintf(stderr,"\t-w safile   write sa to safile\n");    
     fprintf(stderr,"\t-W lcpfile  write lcp to lcpfile\n");
+    fprintf(stderr,"\t-x          read input as sequence of uint16_t values\n");
     fprintf(stderr,"\t-a          compute and print average LCP value\n");   
     fprintf(stderr,"\t-v          produces a verbose output\n\n");
     return 0;
@@ -152,29 +158,51 @@ int main(int argc, char *argv[])
       fprintf(stderr, "%s: file empty\n", fnam);
       return 0;
    }
+   if (input_is_16bit && n % 2 != 0) {
+      fprintf(stderr, "%s: input file size is not a multiple of 2 bytes\n", fnam);
+      return 1;
+   }
+   // allocate space for SA
    p=malloc((size_t) (n+1)*sizeof *p);
-   x=malloc((size_t) (n+1)*sizeof *x);
-   if (! p || ! x) {
+   if (! p) {
       fprintf(stderr, "malloc failed\n");
+      return 1;
+   }
+   // allocate and read text with space for one extra symbol
+   if (input_is_16bit) x=malloc((size_t) (n/2 + 1)*sizeof(uint16_t));
+   else x=malloc((size_t) (n+1)*sizeof(uint8_t));
+   if (! x) {
+      fprintf(stderr, "malloc failed\n");
+      free(p);
       return 1;
    }
    // read text
    rewind(f);
-   if(fread(x, sizeof *x, (size_t)n, f)!=(size_t)n) {
-      perror(fnam);
-      return 1;
+   if(fread(x, sizeof(uint8_t), (size_t)n, f)!=(size_t)n) {
+       perror(fnam);free(x); free(p);
+       return 1;
    }
+   if(input_is_16bit) n /= 2;
 
   /* ---------  start measuring time ------------- */
   start_time = times(&st);
   int32_t e;
-  #ifdef USE_INT64
-  e = libsais64(x, p, n, 1, NULL);
-  #else
-  e = libsais(x, p, n, 1, NULL);
-  #endif
+  if (input_is_16bit) {
+    #ifdef USE_INT64
+    e = libsais16x64((const uint16_t *)x, p, n, 1, NULL);
+    #else
+    e = libsais16((const uint16_t *)x, p, n, 1, NULL);
+    #endif
+  } else {
+    #ifdef USE_INT64
+    e = libsais64((const uint8_t *)x, p, n, 1, NULL);
+    #else
+    e = libsais((const uint8_t *)x, p, n, 1, NULL);
+    #endif
+  }
   if(e<0) {
-    fprintf(stderr,"Error: libsais returned %d\n", e);
+    fprintf(stderr,"Error: suffix array construction returned %d\n", e);
+    free(x); free(p);
     return 1;
   }
   end_time = times(&en);
@@ -194,15 +222,31 @@ int main(int argc, char *argv[])
       return 1;
     }  
     if(Verbose>1) fprintf(stderr,"Computing lcp array\n");
-    #ifdef USE_INT64
-    e = libsais64_plcp(x, p, plcp, n);
-    if(e<0) {fprintf(stderr,"Error: libsais64_plcp returned %d\n", e);return 1;}
-    e = libsais64_lcp(plcp, p, p, n);
-    if(e<0) {fprintf(stderr,"Error: libsais64_lcp returned %d\n", e);return 1;}
-    #else
-    libsais_plcp(x, p, plcp, n);
-    libsais_lcp(plcp, p, p, n);
-    #endif
+    if (input_is_16bit) {
+      #ifdef USE_INT64
+      e = libsais16x64_plcp((const uint16_t *)x, p, plcp, n);
+      if(e<0) {fprintf(stderr,"Error: libsais16x64_plcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      e = libsais16x64_lcp(plcp, p, p, n);
+      if(e<0) {fprintf(stderr,"Error: libsais16x64_lcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      #else
+      e = libsais16_plcp((const uint16_t *)x, p, plcp, n);
+      if(e<0) {fprintf(stderr,"Error: libsais16_plcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      e = libsais16_lcp(plcp, p, p, n);
+      if(e<0) {fprintf(stderr,"Error: libsais16_lcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      #endif
+    } else {
+      #ifdef USE_INT64
+      e = libsais64_plcp((const uint8_t *)x, p, plcp, n);
+      if(e<0) {fprintf(stderr,"Error: libsais64_plcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      e = libsais64_lcp(plcp, p, p, n);
+      if(e<0) {fprintf(stderr,"Error: libsais64_lcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      #else
+      e = libsais_plcp((const uint8_t *)x, p, plcp, n);
+      if(e<0) {fprintf(stderr,"Error: libsais_plcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      e = libsais_lcp(plcp, p, p, n);
+      if(e<0) {fprintf(stderr,"Error: libsais_lcp returned %d\n", e); free(x); free(p); free(plcp); return 1;}
+      #endif
+    }
     end_time = times(&en);
     tot_time =  (end_time - lcp_time)/DBL_CLK_TCK;
     if(Verbose) fprintf(stderr,"Elapsed time: %f seconds (SA+LCP)\n", tot_time);
@@ -211,12 +255,9 @@ int main(int argc, char *argv[])
       write_lcp(lcp_filename, p, n);
     
     if(compute_avg_lcp) {
-      double sum = 0.0;
-      for(idx_t i = 0; i < n; i++) {
-        sum += p[i];
-      }
-      double avg = (n > 0) ? sum / n : 0.0;
-      printf("Average LCP: %f\n", avg);
+      long long sum = 0;
+      for(idx_t i = 0; i < n; i++) sum += p[i];
+      printf("Average LCP: %f\n", (double) sum / n);
     }
     
     free(plcp); 
@@ -226,6 +267,6 @@ int main(int argc, char *argv[])
   // free memory
   free(x); free(p);
   tot_time =  (end_time - start_time)/DBL_CLK_TCK;
-  printf("Elapsed time: %f seconds.\n", tot_time);
+  printf("Elapsed time: %f seconds\n", tot_time);
   return 0;
 }
